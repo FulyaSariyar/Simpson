ff1f5dee1e38d298cb6a2f046a556b3a61b3f918
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-27 21:59:15 +0300

    tasarım & tableview ayarları

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Fulya Sarıyar on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Homer Simpson"
+        return cell
+    }
+}
+
---
+++ SimpsonBook/detailsVC.swift
@@ -0,0 +1,24 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Fulya Sarıyar on 27.06.2022.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
